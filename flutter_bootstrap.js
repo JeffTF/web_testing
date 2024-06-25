a02bf574458390b0dd55b4e3505db1a3a4696642
Ko Ko Maung <122875462+JeffTF@users.noreply.github.com>
2024-06-26 01:40:00 +0630

    some changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "352699702"
+    serviceWorkerVersion: "174761940"
   }
 });
